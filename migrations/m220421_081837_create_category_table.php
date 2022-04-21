a75245d933fecab49886c8cfd08a83508b6ce95a
stmirov <stmirov@gmail.com>
2022-04-21 12:49:51 +0300

    -- Migration 'create_category_table'.

---
+++ migrations/m220421_081837_create_category_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%category}}`.
+ */
+class m220421_081837_create_category_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('category', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string()->notNull()->defaultValue(''),
+            'description' => $this->text()->notNull()->defaultValue(''),
+        ]);
+        $this->batchInsert('category', ['title', 'description'], [
+            ['Ukraine', 'News from Ukraine'],
+            ['Europe', 'News from Europe'],
+            ['World', 'News from World'],
+        ]);
+        echo "\nTest category news: Ukraine, Europe, World \n\n";
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('category');
+    }
+}
