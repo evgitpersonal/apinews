1f41822a8a639221730b412f2e171d8dbb4b5bbf
stmirov <stmirov@gmail.com>
2022-04-21 16:21:43 +0300

    -- CRUD news.

---
+++ models/News.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "news".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $content
+ */
+class News extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'news';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['content'], 'required'],
+            [['content'], 'string'],
+            [['title'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'content' => 'Content',
+        ];
+    }
+}
